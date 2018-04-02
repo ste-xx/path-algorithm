0aafc8f55c70005b4db55573067dcdfd07ade388
ste-xx <stefan.breitenstein@gmail.com>
2018-04-03 00:02:09 +0200

    netlify support

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e # halt script on error
+zip -r website.zip web index.html
+curl -H "Content-Type: application/zip" \
+     -H "Authorization: Bearer $NETLIFYKEY" \
+     --data-binary "@website.zip" \
+     https://api.netlify.com/api/v1/sites/amazing-wright-416994.netlify.com/deploys
